6f5ab30fc550436a4b62f87e39c6c112841de8b1
leckzilla <leckier@hotmail.com>
2020-03-20 11:35:17 +0000

    1st commit

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="pointer">
+      <h1>Alexa Berry</h1>
+      <h3>Birmingham, UK -- Est 1985</h3>
+    </div>
+  );
+}
+
+export default App;
